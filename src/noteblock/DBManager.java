87ff639c09ee2048fb0bb342887fabfbf8d685cb
Hannes Laimer <hannes.laimer@tfobz.net>
2018-02-01 14:18:19 +0100

    db manager class

--- src/noteblock/DBManager.java
+++ src/noteblock/DBManager.java
@@ -1,5 +1,23 @@
 package noteblock;
 
 
+import com.mysql.jdbc.jdbc2.optional.MysqlConnectionPoolDataSource;
+
 public class DBManager {
+    private static MysqlConnectionPoolDataSource dbObject = null;
+
+    private void establish_connection() {
+        dbObject = new MysqlConnectionPoolDataSource();
+        dbObject.setUser("root");
+        dbObject.setPassword("");
+        dbObject.setServerName("localhost");
+        dbObject.setDatabaseName("notedb");
+    }
+
+    public MysqlConnectionPoolDataSource getDbObject() {
+        if (dbObject == null)
+            establish_connection();
+        return dbObject;
+    }
+
 }
